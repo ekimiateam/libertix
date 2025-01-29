fd6d7aa01d0043bbad6106df2b76b6fadf2062c6
MopigamesYT <mopigames2.0@gmail.com>
2025-01-29 17:56:05 +0100

    Add ApplyChanges page with basic layout and code-behind

---
+++ Pages/ApplyChanges.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Libertix.Pages
+{
+    public partial class ApplyChanges : Page
+    {
+        public ApplyChanges()
+        {
+            InitializeComponent();
+        }
+    }
+}
