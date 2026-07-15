d234550f2589c35b1163774c66dfd04486ea0536
felix068 <77407722+felix068@users.noreply.github.com>
2026-07-15 15:40:20 +0200

    Update CI actions and production filepool endpoint
    Pin the historical workflow to Node 24-compatible releases of checkout, setup-python, setup-msbuild, and upload-artifact. Switch Libertix's single filepool BaseUrl from the local test API to https://ekimia.fr/libertix.

--- Helpers/FilepoolConfig.cs
+++ Helpers/FilepoolConfig.cs
@@ -2,8 +2,8 @@ namespace Libertix.Helpers
 {
     public static class FilepoolConfig
     {
-        public const string BaseUrl = "http://192.168.1.170:8000/filepool";
-        ///public const string BaseUrl = "https://ekimia.fr/libertix";
+        ///public const string BaseUrl = "http://192.168.1.170:8000/filepool";
+        public const string BaseUrl = "https://ekimia.fr/libertix";
 
         public static string DistrosUrl => BaseUrl + "/distros.json";
 
